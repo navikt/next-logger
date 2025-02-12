72f44f876655177a3948fd18e0d37c0b4170d89b
Martin Almvik <martin.almvik@nav.no>
2025-02-12 17:33:09 +0100

    Add support for secure logs

--- src/logger.ts
+++ src/logger.ts
@@ -5,6 +5,8 @@ import { logger as frontendLogger } from './frontendLogger'
 
 export const logger = typeof window !== 'undefined' ? frontendLogger() : backendLogger()
 
+export const secureLogger = typeof window !== 'undefined' ? frontendLogger(true) : backendLogger({ secure: true })
+
 export const createChildLogger = (requestId: string): pino.Logger => {
     return logger.child({ x_request_id: requestId })
 }
